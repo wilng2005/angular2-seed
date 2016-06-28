eac828c3aa7d454763ba74292c4072b8bca8d487
Wil Ng <wil.ng.2005@gmail.com>
2016-06-28 13:33:12 +0800

    Code added for lecture 47 v2

--- app/list-item.js
+++ app/list-item.js
@@ -7,5 +7,4 @@ System.register([], function(exports_1, context_1) {
         }
     }
 });
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJsaXN0LWl0ZW0uanMiLCJzb3VyY2VzQ29udGVudCI6W10sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
+//# sourceMappingURL=list-item.js.map
--- app/shopping-list/mock-shopping-list.js
+++ app/shopping-list/mock-shopping-list.js
@@ -9,5 +9,4 @@ System.register([], function(exports_1, context_1) {
         }
     }
 });
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNob3BwaW5nLWxpc3QvbW9jay1zaG9wcGluZy1saXN0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztRQUVXLGFBQWE7Ozs7WUFBYiwyQkFBQSxhQUFhLEdBQW1CLElBQUksS0FBSyxFQUFZLENBQUEsQ0FBQyIsImZpbGUiOiJzaG9wcGluZy1saXN0L21vY2stc2hvcHBpbmctbGlzdC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7TGlzdEl0ZW19IGZyb20gXCIuLi9saXN0LWl0ZW1cIjtcblxuZXhwb3J0IGxldCBzaG9wcGluZ19saXN0OkFycmF5PExpc3RJdGVtPiA9IG5ldyBBcnJheTxMaXN0SXRlbT4oKTsiXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
+//# sourceMappingURL=mock-shopping-list.js.map
